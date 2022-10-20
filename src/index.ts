72d86279fb9563519913df888f8b7a656f7edd95
Bram Borggreve <beeman@beeman.nl>
2022-10-20 21:48:33 +0100

    chore: switch to Nx Workspace

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv'
-import { server } from './server/server'
-import { getServerConfig, ServerConfig } from './server/server-config'
-
-dotenv.config()
-
-const config: ServerConfig = getServerConfig()
-
-server(config).catch((e) => console.error(`An error occurred:`, e))
